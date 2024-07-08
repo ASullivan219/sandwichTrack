83db41d9c11a14c0fe384b66c71fb001690d3f74
Alex <a.sullivan219@protonmail.com>
2024-07-07 22:45:48 -0700

    added notifier setup to main

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"os"
+
 	"github.com/ASullivan219/freeSandwich/internal/cronmanager"
 	"github.com/ASullivan219/freeSandwich/internal/lafc"
+	"github.com/ASullivan219/freeSandwich/internal/notifier"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
 	cronMan := cronmanager.New()
+	emailNotifier := notifier.NewEmailNotifier(
+		os.Getenv("FROM_EMAIL"),
+		os.Getenv("EMAIL_PASSWORD"),
+		os.Getenv("EMAIL_HOST"),
+		os.Getenv("EMAIL_PORT"),
+	)
+
 	for _, job := range lafc.BuildCronnables() {
 		cronMan.AddJob(job.Sf.ExecutionTime, &job, job.Sf.Description)
 	}
